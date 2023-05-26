b7527868a437cce6d884e6f86d51236d07784b3b
avi-1227 <avinaashprasadd@gmail.com>
2023-05-27 01:58:40 +0530

    add a function in Post Model get_absolute_urls()

--- blogapp/models.py
+++ blogapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.query import QuerySet
 from django.utils import timezone
+from django.urls import reverse # New
 
 # Create your models here.
 
@@ -15,8 +16,6 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
     
-
-
     title=models.CharField(max_length=250)
     slug = models.CharField(max_length=250)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
@@ -28,6 +27,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blogapp:post_detail', args=['self.id'])
 
     objects = models.Manager()
     published = PublishedManager()
