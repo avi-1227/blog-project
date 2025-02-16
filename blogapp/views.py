c7582a851bf0cf6466046832ec103ac791fbefb3
avi-1227 <avinaashprasadd@gmail.com>
2025-02-16 20:16:50 +0530

    Add search functionality

--- blogapp/views.py
+++ blogapp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_POST
 from .forms import CommentForm
 from .models import Post, Comment
+from django.db.models import Q
 # Create your views here.
 
 def post_list(request):
@@ -12,6 +13,16 @@ def post_list(request):
     page_number = request.GET.get('page', 1)
     posts = paginator.page(page_number)
     return render(request, 'blogapp/post/list.html', {'posts':posts})
+
+
+def search_results(request):
+    query = request.GET.get('q')
+    posts = Post.published.all()
+    if query:
+        posts = posts.filter(Q(title__icontains=query)|Q(body__icontains=query))
+    
+    return render(request, 'blogapp/post/showsearch.html', {'posts':posts, 'query':query})
+
 
 
 def post_detail(request, year, month, day, post):
@@ -30,13 +41,4 @@ def post_detail(request, year, month, day, post):
     return render(request, 'blogapp/post/detail.html', {'post':post, 'comments':comments, 'form':form})
 
 
-@require_POST
-def post_comment(request, post_id):
-    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED) 
-    comment = None 
-    form = CommentForm(data=request.POST)
-    if form.is_valid():
-        comment = form.save(commit=False)
-        comment.post = post
-        comment.save()
-    return render(request, 'blogapp/post/detail.html', {'posts':posts, 'form':form, 'comment':comment})  
+ 
